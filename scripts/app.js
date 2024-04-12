6a1c6006a7e3b49f4b9742e0dac75ec1003c26d0
JamesGlik <sadoiani9900@gmail.com>
2024-04-12 16:14:43 +0400

    assignment

---
+++ scripts/app.js
@@ -0,0 +1,53 @@
+//1
+
+function even(number) {
+    return number % 2 === 0;
+}
+
+console.log(even(1));
+console.log(even(2));
+
+//////////////////////////////////////////////////////////////////
+
+
+//2
+
+function rectangleArea(length, height) {
+    return length * height;
+}
+
+console.log(rectangleArea(3, 7));
+
+//////////////////////////////////////////////////////////////////
+
+
+//3
+
+//function rectangleArea(a) {
+    //return a * a;
+//}
+
+//console.log(rectangleArea(3));
+
+//////////////////////////////////////////////////////////////////
+
+
+//4
+
+function randomNumber() {
+    return Math.floor(Math.random() * 101);
+}
+
+console.log(randomNumber());
+
+//////////////////////////////////////////////////////////////////
+
+
+//5
+
+//mexute aris characters.js fileshi, aq rom vwerdi ar mushaobda
+
+
+
+
+
